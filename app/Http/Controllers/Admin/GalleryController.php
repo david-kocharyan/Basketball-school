1541b0e7d4955866498d01cb3beddb0ac0e65e77
David <david@aimtech.am>
2020-04-20 13:52:58 +0400

    home gallery fix

--- app/Http/Controllers/Admin/GalleryController.php
+++ app/Http/Controllers/Admin/GalleryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Gallery;
 use App\GalleryImage;
+use App\HomeGallery;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -21,7 +22,6 @@ class GalleryController extends Controller
 
     /**
      * Display a listing of the resource.
-     *
      * @return \Illuminate\Http\Response
      */
     public function index()
@@ -29,12 +29,11 @@ class GalleryController extends Controller
         $gallery = Gallery::all();
         $route = self::ROUTE;
         $title = self::TITLE;
-        return view(self::FOLDER.".index", compact('route', 'title', 'gallery'));
+        return view(self::FOLDER . ".index", compact('route', 'title', 'gallery'));
     }
 
     /**
      * Show the form for creating a new resource.
-     *
      * @return \Illuminate\Http\Response
      */
     public function create()
@@ -46,8 +45,7 @@ class GalleryController extends Controller
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -65,7 +63,7 @@ class GalleryController extends Controller
 
         if ($gallery->id) {
             foreach ($request->image as $key) {
-                $product_image_name =  Storage::disk('public')->put('gallery', $key);
+                $product_image_name = Storage::disk('public')->put('gallery', $key);
                 $image = new GalleryImage;
                 $image->gallery_id = $gallery->id;
                 $image->name = basename($product_image_name);
@@ -78,20 +76,18 @@ class GalleryController extends Controller
 
     /**
      * Display the specified resource.
-     *
-     * @param  \App\Gallery  $gallery
+     * @param \App\Gallery $gallery
      * @return \Illuminate\Http\Response
      */
     public function show(Gallery $gallery)
     {
         $title = 'Show ' . self::TITLE;
-        return view(self::FOLDER . ".show", compact( 'title', 'gallery'));
+        return view(self::FOLDER . ".show", compact('title', 'gallery'));
     }
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  \App\Gallery  $gallery
+     * @param \App\Gallery $gallery
      * @return \Illuminate\Http\Response
      */
     public function edit(Gallery $gallery)
@@ -103,9 +99,8 @@ class GalleryController extends Controller
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Gallery  $gallery
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Gallery             $gallery
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Gallery $gallery)
@@ -122,7 +117,7 @@ class GalleryController extends Controller
 
         if ($request->image) {
             foreach ($request->image as $key) {
-                $product_image_name =  Storage::disk('public')->put('gallery', $key);
+                $product_image_name = Storage::disk('public')->put('gallery', $key);
                 $image = new GalleryImage;
                 $image->gallery_id = $gallery->id;
                 $image->name = basename($product_image_name);
@@ -135,8 +130,7 @@ class GalleryController extends Controller
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  \App\Gallery  $gallery
+     * @param \App\Gallery $gallery
      * @return \Illuminate\Http\Response
      */
     public function destroy(Gallery $gallery)
@@ -146,6 +140,13 @@ class GalleryController extends Controller
                 Storage::disk('public')->delete("gallery/$key->name");
             }
         }
+
+        $home = HomeGallery::where('album_id', $gallery->id)->get();
+        foreach ($home as $key) {
+            Storage::disk('public')->delete("home_gallery/$key->image");
+            HomeGallery::destroy($key->id);
+        }
+
         Gallery::destroy($gallery->id);
 
         return redirect(self::ROUTE);
@@ -161,6 +162,6 @@ class GalleryController extends Controller
         $image = GalleryImage::find($id);
         Storage::disk('public')->delete("gallery/$image->name");
         GalleryImage::destroy($image->id);
-        return redirect(self::ROUTE.'/'.$gallery.'/edit');
+        return redirect(self::ROUTE . '/' . $gallery . '/edit');
     }
 }
